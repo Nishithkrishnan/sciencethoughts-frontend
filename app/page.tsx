d44e22497c1dace5de99ff01efb9629005b64da4
Nishithkrishnan <nishithmanu@gmail.com>
2026-04-18 23:27:20 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 // app/page.tsx (Server Component)
 import PostsClient from "./PostsClient";
 
+export const dynamic = 'force-dynamic';
 export default async function Home() {
   // Fetching from your WordPress API
   const res = await fetch("https://sciencethoughts.com/wp-json/wp/v2/posts?_embed");
