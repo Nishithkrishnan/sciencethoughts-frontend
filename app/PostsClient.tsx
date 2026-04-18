0b514ac1e7d9028133186ff84fec8ed9a3674bce
Nishithkrishnan <nishithmanu@gmail.com>
2026-04-18 22:45:06 +0530

    Update PostsClient.tsx

--- app/PostsClient.tsx
+++ app/PostsClient.tsx
@@ -69,7 +69,7 @@ export default function PostsClient({ posts }: any) {
   const fadeInUp = {
     hidden: { opacity: 0, y: 30 },
     visible: { opacity: 1, y: 0, transition: { duration: 0.6, ease: "easeOut" } },
-  };
+  }as const;
 
   return (
     <main style={styles.main}>
